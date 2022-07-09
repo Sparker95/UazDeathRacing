10241de4186878c5a457159e01a9c2267a95c984
zalexki <haumalex@gmail.com>
2022-07-09 04:52:53 +0200

    added particle to boost trigger

--- UazDeathRacing/scripts/Game/UDR/GameMode/UDR_GameMode.c
+++ UazDeathRacing/scripts/Game/UDR/GameMode/UDR_GameMode.c
@@ -16,11 +16,13 @@ class UDR_GameMode: SCR_BaseGameMode
 			"{1A20D130A03F9CF1}Prefabs/Vehicles/Wheeled/UAZ469/UAZ469_Armed.et",
 			"{21C45FA677BCDBDA}Prefabs/Vehicles/Wheeled/M998/M998_Armed.et"
 		};
-		
+
 		Resource res = Resource.Load(vehiclePrefabs[0]);
 		EntitySpawnParams spawnParams = new EntitySpawnParams();
 		IEntity newVehicleEntity = GetGame().SpawnEntityPrefab(res, params: spawnParams);
 		newVehicleEntity.SetWorldTransform(playerPosition);
+		newVehicleEntity.SetName("Vehicle_"+playerId);
+		
 		Vehicle veh = Vehicle.Cast(newVehicleEntity);
 		
 		SCR_CompartmentAccessComponent compartmentAccessComponent = SCR_CompartmentAccessComponent.Cast(controlledEntity.FindComponent(SCR_CompartmentAccessComponent));
@@ -28,5 +30,26 @@ class UDR_GameMode: SCR_BaseGameMode
 		
 		CarControllerComponent m_pCarController = CarControllerComponent.Cast(veh.FindComponent(CarControllerComponent));
 		m_pCarController.StartEngine();
+		
+		array<SCR_ParticleEmitter> emittersList = findEmitters(newVehicleEntity);
+		foreach (SCR_ParticleEmitter emitter : emittersList)
+			emitter.Stop();
     }
+	
+	array<SCR_ParticleEmitter> findEmitters(IEntity vehicle) 
+	{
+		array<SCR_ParticleEmitter> emitters = new array<SCR_ParticleEmitter>;
+		IEntity child = vehicle.GetChildren();
+		while (child)
+		{
+			SCR_ParticleEmitter childSlot = SCR_ParticleEmitter.Cast(child);
+			if (childSlot)
+				if (childSlot.GetPathToPTC() == UDR_BoostVehicleTrigger.BOOST_PARTICLE_NAME)
+					emitters.Insert(childSlot);
+			
+			child = child.GetSibling();
+		}
+		
+		return emitters;
+	}
 }
--- UazDeathRacing/scripts/Game/UDR/Triggers/UDR_BoostVehicleTrigger.c
+++ UazDeathRacing/scripts/Game/UDR/Triggers/UDR_BoostVehicleTrigger.c
@@ -4,25 +4,50 @@ class UDR_BoostVehicleTriggerClass: ScriptedGameTriggerEntityClass
 
 class UDR_BoostVehicleTrigger: ScriptedGameTriggerEntity
 {
-    override void OnActivate(IEntity ent)
+	static const string BOOST_PARTICLE_NAME = "{575C2DEE390847F1}boostParticle.ptc";
+
+	override void OnActivate(IEntity ent)
 	{
 		PrintString("activateBoost");
 
-        Vehicle vehicle = Vehicle.Cast(ent);
+        Vehicle vehicle = Vehicle.Cast(ent);	
         VehicleBaseSimulation simulation = VehicleBaseSimulation.Cast(vehicle.FindComponent(VehicleBaseSimulation));
         VehicleWheeledSimulation vWheelSimu = VehicleWheeledSimulation.Cast(simulation);
         
         vWheelSimu.EngineSetPeakTorqueState(200);
         vWheelSimu.EngineSetPeakPowerState(250);
 		
-		GetGame().GetCallqueue().CallLater(RemoveBoost, 2000, false, vWheelSimu);
+		array<SCR_ParticleEmitter> emittersList;
+		emittersList = findEmitters(ent);
+		foreach (SCR_ParticleEmitter emitter : emittersList)
+			emitter.Play();
+		GetGame().GetCallqueue().CallLater(RemoveBoost, 2000, false, vWheelSimu, emittersList);
     }
 	
-	void RemoveBoost(VehicleWheeledSimulation vWheelSimu)
+	void RemoveBoost(VehicleWheeledSimulation vWheelSimu, array<SCR_ParticleEmitter> emittersList)
 	{
 		PrintString("RemoveBoost");
 		
 		vWheelSimu.EngineSetPeakTorqueState(80);
         vWheelSimu.EngineSetPeakPowerState(180);
+		foreach (SCR_ParticleEmitter emitter : emittersList)
+			emitter.Stop();
+	}
+	
+	array<SCR_ParticleEmitter> findEmitters(IEntity vehicle) 
+	{
+		array<SCR_ParticleEmitter> emitters = new array<SCR_ParticleEmitter>;
+		IEntity child = vehicle.GetChildren();
+		while (child)
+		{
+			SCR_ParticleEmitter childSlot = SCR_ParticleEmitter.Cast(child);
+			if (childSlot)
+				if (childSlot.GetPathToPTC() == UDR_BoostVehicleTrigger.BOOST_PARTICLE_NAME)
+					emitters.Insert(childSlot);
+			
+			child = child.GetSibling();
+		}
+		
+		return emitters;
 	}
 }
