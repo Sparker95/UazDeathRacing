9ac6cd27f8eb8b2077f7b40c88c28ab916e81eee
Saveliy Tronza <sparker_95@mail.ru>
2022-08-06 19:58:43 +0200

    Added basic functionality to deploy mines

--- UazDeathRacing/scripts/Game/UDR/UDR_WeaponManagerComponent.c
+++ UazDeathRacing/scripts/Game/UDR/UDR_WeaponManagerComponent.c
@@ -13,7 +13,14 @@ class UDR_WeaponManagerComponentClass : ScriptComponentClass
 }
 
 class UDR_WeaponManagerComponent : ScriptComponent
-{
+{	
+	[Attribute()]
+	protected vector m_vDeployablePosition;
+	
+	[RplProp()]
+	protected int m_iDeployableCount;
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
 	void RpcAsk_AddWeapon(UDR_Weapons weaponId)
 	{
@@ -38,6 +45,7 @@ class UDR_WeaponManagerComponent : ScriptComponent
 		RpcDo_BroadcastSetSlotWeapon(newRplId, true);
 	}
 	
+	//----------------------------------------------------------------------------------------------------------------------------------------
 	[RplRpc(RplChannel.Reliable, RplRcver.Broadcast)]
 	protected void RpcDo_BroadcastSetSlotWeapon(RplId newWeaponId, bool deletePrevWeapon)
 	{
@@ -61,12 +69,13 @@ class UDR_WeaponManagerComponent : ScriptComponent
 			SCR_Global.DeleteEntityAndChildren(prevWeaponEnt);
 	}
 	
+	//----------------------------------------------------------------------------------------------------------------------------------------
 	void Owner_RequestAddWeapon(int weaponId)
 	{
 		Rpc(RpcAsk_AddWeapon, weaponId);
 	}
 	
-	
+	//----------------------------------------------------------------------------------------------------------------------------------------
 	// Returns true when this vehicle is full on ammo
 	bool IsFullAmmo()
 	{
@@ -93,4 +102,78 @@ class UDR_WeaponManagerComponent : ScriptComponent
 		
 		return false;
 	}
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+	// Deployables
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+	void Owner_RequestFireDeployable()
+	{
+		IEntity owner = GetOwner();
+		vector myTransform[4]; // aside, up, dir, pos
+		owner.GetTransform(myTransform);
+		
+		
+		// Trace move downwards from deployable position to find where to place the mine
+		TraceParam tp = new TraceParam();
+		tp.Start = owner.CoordToParent(m_vDeployablePosition);
+		tp.End = tp.Start - 2.0 * myTransform[1]; // Downwards
+		tp.Flags = TraceFlags.ENTS | TraceFlags.WORLD | TraceFlags.ANY_CONTACT;
+		tp.Exclude = owner; // Exclude our car
+		float traceProgress = GetGame().GetWorld().TraceMove(tp, null);
+		if (traceProgress >= 1.0)
+			return; // Invalid trace, didn't hit anything
+		
+		if (tp.TraceEnt && Vehicle.Cast(tp.TraceEnt))
+			return; // We've hit a car, can't deploy here
+		
+		// Find the position where to place the deployable
+		// It must be placed according to the surface normal
+		vector pos = (1 - traceProgress) * tp.Start + traceProgress*tp.End;
+		
+		vector vUp = tp.TraceNorm;
+		vUp.Normalize();
+		
+		vector vDir;
+		if (vector.Distance(vUp, Vector(0, 1, 0)) > 0.01 &&
+			vector.Distance(vUp, Vector(0, -1, 0)) > 0.01)
+		{
+			vDir = (Vector(0, 1, 0)) * vUp;
+		}
+		else
+			vDir = Vector(1, 0, 0);
+		vDir.Normalize();
+		
+		vector vAside = vUp * vDir;
+		vAside.Normalize();
+		
+		Rpc(RpcAsk_FireDeployable, vAside, vUp, vDir, pos);
+	}
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+	[RplRpc(RplChannel.Reliable, RplRcver.Server)]
+	void RpcAsk_FireDeployable(vector vAside, vector vUp, vector vDir, vector pos)
+	{
+		EntitySpawnParams p = new EntitySpawnParams();
+		p.Transform[0] = vAside;
+		p.Transform[1] = vUp;
+		p.Transform[2] = vDir;
+		p.Transform[3] = pos;
+		Resource res = Resource.Load("{2D8669E6D1E24B4E}Prefabs/DirtPile/DeployableDirtPile.et");
+		IEntity deployableEntity = GetGame().SpawnEntityPrefab(res, params: p);
+		
+		// Later delete it
+		GetGame().GetCallqueue().CallLater(SCR_EntityHelper.DeleteEntityAndChildren, 20000, false, deployableEntity);
+	}
+	
+	
+	
+	//----------------------------------------------------------------------------------------------------------------------------------------
+	override void _WB_AfterWorldUpdate(IEntity owner, float timeSlice)
+	{
+		// Visualize the deployable position
+		vector deployablePosWorld = GetOwner().CoordToParent(m_vDeployablePosition);
+		Shape.CreateSphere(Color.RED, ShapeFlags.ONCE, deployablePosWorld, 0.05);
+	}
+	
 }
